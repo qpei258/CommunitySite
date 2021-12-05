130e93008263f08d2794accb3b841c4abdce052e
qpei258 <81565492+qpei258@users.noreply.github.com>
2021-12-05 18:45:04 +0900

    Add files via upload
    member data

---
+++ CommunitySite/src/main/java/model/MemberDAO.java
@@ -0,0 +1,17 @@
+package model;
+
+public class MemberDAO {
+	private static MemberDAO instance = new MemberDAO();
+	
+	public static MemberDAO getInstance() {
+		return instance;
+	}
+	
+	public void addMember() {
+		
+	}
+	
+	public String memberLogin() {
+		return "";
+	}
+}
---
+++ CommunitySite/src/main/java/model/MemberDTO.java
@@ -0,0 +1,58 @@
+package model;
+
+public class MemberDTO {
+	String name;
+	String id;
+	String password;
+	String phone;
+	String email;
+	String address;
+	
+	public MemberDTO(String name, String id, String password, String phone, String email, String address) {
+		this.name = name;
+		this.id = id;
+		this.password = password;
+		this.phone = phone;
+		this.email = email;
+		this.address = address;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
+}
