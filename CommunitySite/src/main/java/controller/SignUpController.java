04fa46853ee5015538596f17b14a3d9fc43ec068
qpei258 <81565492+qpei258@users.noreply.github.com>
2021-12-08 20:00:59 +0900

    Update SignUpController.java
    db syc

--- CommunitySite/src/main/java/controller/SignUpController.java
+++ CommunitySite/src/main/java/controller/SignUpController.java
@@ -25,7 +25,7 @@ public class SignUpController extends HttpServlet {
 		
 		String name = request.getParameter("name");
 		String id = request.getParameter("id");
-		String password = request.getParameter("password");
+		String password = request.getParameter("passwd");
 		String phone1 = request.getParameter("phone1");
 		String phone2 = request.getParameter("phone2");
 		String phone3 = request.getParameter("phone3");
