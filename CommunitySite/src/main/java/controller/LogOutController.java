14c90aebb18807f006d8df1645aad93cd865ed7d
qpei258 <81565492+qpei258@users.noreply.github.com>
2022-01-18 09:50:25 +0900

    Add files via upload
    로그아웃 추가

---
+++ CommunitySite/src/main/java/controller/LogOutController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogOutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		request.setCharacterEncoding("utf-8");
+		
+		HttpSession session = request.getSession();
+		
+		session.setAttribute("login", null);
+		session.setAttribute("userId", null);
+		session.setAttribute("userpass", null);
+		response.sendRedirect("Main.jsp");
+	}
+}
